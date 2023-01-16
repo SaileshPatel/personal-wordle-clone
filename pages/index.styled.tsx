cd6600afdfbe03274473ce6bf08cd8aa6d49fa5d
Sailesh Patel <saileshapatel@gmail.com>
2023-01-16 21:07:58 +0000

    Replace CSS modules with styled-components

---
+++ pages/index.styled.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const headerFonts = "Verdana, Arial, Helvetica, sans-serif";
+const paragraphFonts = "Helvetica, Verdana, Arial, sans-serif";
+
+export const TitleH1 = styled.h1`
+  font-family: ${headerFonts};
+  font-size: 3.5em;
+`;
+
+export const Paragraph = styled.p`
+  font-family: ${paragraphFonts};
+  font-size: 1.2em;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
+import { TitleH1, Paragraph } from "./index.styled";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +15,14 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main className={styles.main}>
-        <h1>Personal Wordle Clone</h1>
+      <main>
+        <TitleH1>Personal Wordle Clone</TitleH1>
+        <Paragraph>Welcome to this Wordle clone.</Paragraph>
+        <Paragraph>
+          The main purpose of this application is to learn more about how to
+          create a React project from scratch, how to structure components, and
+          how development work generally on a complex project, like Wordle.
+        </Paragraph>
       </main>
     </>
   );
