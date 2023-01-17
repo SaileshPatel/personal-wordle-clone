f669ddff926a30743ccb3edbc88cda826400e2be
Sailesh Patel <saileshapatel@gmail.com>
2023-01-17 20:22:18 +0000

    Create styled component for Letter component

---
+++ components/atoms/letter/letter.styled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const size = "4rem";
+
+export const StyledLetterInput = styled.input`
+  width: ${size};
+  height: ${size};
+  font-size: ${size};
+  caret-color: transparent;
+  text-align: center;
+  background-color: #0101;
+`;
--- components/atoms/letter/letter.tsx
+++ components/atoms/letter/letter.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import { StyledLetterInput } from "./letter.styled";
 
 export default function Letter() {
   const [letterEntered, setLetterEntered] = useState<string>("");
   return (
-    <input
+    <StyledLetterInput
       type="text"
       minLength={1}
       maxLength={1}
