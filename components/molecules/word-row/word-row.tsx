7e512f2464c08100300114aec28b4c0f2163f2b2
Sailesh Patel <saileshapatel@gmail.com>
2023-02-18 15:17:48 +0000

    Generate loop of Letter components as WordRow component

--- components/molecules/word-row/word-row.tsx
+++ components/molecules/word-row/word-row.tsx
@@ -1,6 +1,12 @@
-export default function WordRow() {
+import Letter from "../../atoms";
+import { TWordRowProps } from "./word-row.types";
+
+export default function WordRow({ wordSize }: TWordRowProps) {
   return (
     <>
+      {[...Array(wordSize)].map((x, i) => 
+        <Letter key={i} />
+      )}
     </>
-  );
+  )
 }
