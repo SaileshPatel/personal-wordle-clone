82ae16e24ae5a81473a2ceeeb8985dcd542ceac0
Sailesh Patel <saileshapatel@gmail.com>
2023-02-18 18:58:23 +0000

    Create state array to hold completed word

--- components/molecules/word-row/word-row.tsx
+++ components/molecules/word-row/word-row.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import Letter from "../../atoms";
 import { TWordRowProps } from "./word-row.types";
 
 export default function WordRow({ wordSize }: TWordRowProps) {
+  const [word, setWord] = useState<string[]>();
+
   return (
     <>
       {[...Array(wordSize)].map((x, i) => (
