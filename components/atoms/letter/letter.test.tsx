cbad9b05bffa8f72ddb4e96af2adb5dadc4f10d1
Sailesh Patel <saileshapatel@gmail.com>
2023-02-18 18:26:20 +0000

    Install jest testing (#4)
    * Install jest packages
    
    * Add jest config and setup files
    
    * Create initial unit tests

---
+++ components/atoms/letter/letter.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import Letter from "./letter";
+
+describe("Letter", () => {
+  it("should be truthy", () => {
+    const renderedItem = render(<Letter />);
+    expect(renderedItem).toBeTruthy();
+  });
+});
+describe("when a letter is entered", () => {
+  it("should display in the text box", async () => {
+    const renderedItem = render(<Letter />);
+    const input = renderedItem.getByLabelText("letter");
+
+    fireEvent.change(input, { target: { value: "A" } });
+    expect(screen.getByDisplayValue("A")).toBeInTheDocument();
+  });
+});
--- components/atoms/letter/letter.tsx
+++ components/atoms/letter/letter.tsx
@@ -5,6 +5,7 @@ export default function Letter() {
   const [letterEntered, setLetterEntered] = useState<string>("");
   return (
     <StyledLetterInput
+      aria-label="letter"
       type="text"
       minLength={1}
       maxLength={1}
