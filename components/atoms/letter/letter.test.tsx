a020cbfae7166febce41e45542c488b79478c95f
Sailesh Patel <saileshapatel@gmail.com>
2023-02-18 19:47:13 +0000

    Add functionality to create string array of words

--- components/atoms/letter/letter.test.tsx
+++ components/atoms/letter/letter.test.tsx
@@ -3,13 +3,17 @@ import Letter from "./letter";
 
 describe("Letter", () => {
   it("should be truthy", () => {
-    const renderedItem = render(<Letter />);
+    const renderedItem = render(
+      <Letter letterPlace={1} placeLetterInWord={jest.fn()} />
+    );
     expect(renderedItem).toBeTruthy();
   });
 });
 describe("when a letter is entered", () => {
   it("should display in the text box", async () => {
-    const renderedItem = render(<Letter />);
+    const renderedItem = render(
+      <Letter letterPlace={1} placeLetterInWord={jest.fn()} />
+    );
     const input = renderedItem.getByLabelText("letter");
 
     fireEvent.change(input, { target: { value: "A" } });
--- components/molecules/word-row/word-row.tsx
+++ components/molecules/word-row/word-row.tsx
@@ -3,12 +3,22 @@ import Letter from "../../atoms";
 import { TWordRowProps } from "./word-row.types";
 
 export default function WordRow({ wordSize }: TWordRowProps) {
-  const [word, setWord] = useState<string[]>();
+  const [word, setWord] = useState<string[]>([]);
 
+  const placeLetterInWord = (letter: string, index: number) => {
+    setWord([]);
+    const newWord = [...word?.slice(0, index), letter, ...word?.slice(index)];
+    setWord(newWord);
+  };
+
+  /*
+   * TODO:
+   * Add functionality to remove letter when going back
+   */
   return (
     <>
       {[...Array(wordSize)].map((x, i) => (
-        <Letter key={i} />
+        <Letter key={i} letterPlace={i} placeLetterInWord={placeLetterInWord} />
       ))}
     </>
   );
