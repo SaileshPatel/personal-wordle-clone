e4f0d612789706994dd33a221af2fc2c1ddd1221
Sailesh Patel <saileshapatel@gmail.com>
2023-02-18 16:00:24 +0000

    Add linting pre commit hook (#3)
    * Add lint-staged to lint code before commits
    
    * Use pre-commit lint to lint unlinted files

--- components/molecules/word-row/word-row.tsx
+++ components/molecules/word-row/word-row.tsx
@@ -4,9 +4,9 @@ import { TWordRowProps } from "./word-row.types";
 export default function WordRow({ wordSize }: TWordRowProps) {
   return (
     <>
-      {[...Array(wordSize)].map((x, i) => 
+      {[...Array(wordSize)].map((x, i) => (
         <Letter key={i} />
-      )}
+      ))}
     </>
-  )
+  );
 }
