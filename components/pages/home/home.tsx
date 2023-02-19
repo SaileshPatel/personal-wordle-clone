5159c73668996347a0c9cecd5594fbd1fc608a90
Sailesh Patel <saileshapatel@gmail.com>
2023-02-19 18:16:55 +0000

    Add error messages when word is not recognised or not able to be checked (#10)
    * Set initial isWordValid state to true to avoid displaying error message on page load
    
    * Create styled div component to stylise error messages
    
    * Add error messages and styles when word is not found or not checked

--- components/pages/home/home.tsx
+++ components/pages/home/home.tsx
@@ -1,12 +1,12 @@
 import Head from "next/head";
 import { useState } from "react";
 import WordRow from "../../molecules/";
-import { TitleH1, Paragraph } from "./home.styled";
+import { TitleH1, Paragraph, ErrorMessageDiv } from "./home.styled";
 
 export default function Home() {
   const [isWordCheckSuccessful, setIsWordCheckSuccessful] =
     useState<boolean>(true);
-  const [isWordValid, setIsWordValid] = useState<boolean>(false);
+  const [isWordValid, setIsWordValid] = useState<boolean>(true);
 
   return (
     <>
@@ -26,17 +26,23 @@ export default function Home() {
           create a React project from scratch, how to structure components, and
           how development work generally on a complex project, like Wordle.
         </Paragraph>
-        {!isWordCheckSuccessful ? (
-          <Paragraph>
-            We could not check the word you entered unfortunately. Please try
-            again later.
-          </Paragraph>
-        ) : null}
         <WordRow
           wordSize={5}
           setIsWordCheckSuccessful={setIsWordCheckSuccessful}
           setIsWordValid={setIsWordValid}
         />
+        {!isWordCheckSuccessful ? (
+          <ErrorMessageDiv>
+            We could not check the word you entered unfortunately. Please try
+            another word or try again later.
+          </ErrorMessageDiv>
+        ) : null}
+        {!isWordValid ? (
+          <ErrorMessageDiv>
+            We do not recognise the word you have entered. Please try another
+            word.
+          </ErrorMessageDiv>
+        ) : null}
       </main>
     </>
   );
