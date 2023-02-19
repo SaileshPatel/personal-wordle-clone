58bd39b61ca20fcba13ef9d32c508b0109d5a04b
Sailesh Patel <saileshapatel@gmail.com>
2023-02-19 03:17:51 +0000

    Create initial function to check word validity

--- components/molecules/word-row/word-row.tsx
+++ components/molecules/word-row/word-row.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { checkWordValidity } from "../../../services/dictionary-api/dictionary-api";
 import Letter from "../../atoms";
 import { TWordRowProps } from "./word-row.types";
 
 export default function WordRow({ wordSize }: TWordRowProps) {
   const [word, setWord] = useState<string[]>([]);
   const [isWordValid, setIsWordValid] = useState<boolean>(false);
+  const [isWordCheckSuccessful, setIsWordCheckSuccessful] = useState<boolean>();
 
   const placeLetterInWord = (letter: string, index: number) => {
     setWord([]);
@@ -15,6 +17,21 @@ export default function WordRow({ wordSize }: TWordRowProps) {
         });
 
     setWord(newWord);
+  };
+
+  const checkIfWordIsValid = async (word: string[]) => {
+    const response = await checkWordValidity(word.toString());
+    if (
+      response.isWordValid?.valueOf() === undefined ||
+      response.status > 404
+    ) {
+      setIsWordValid(false);
+      setIsWordCheckSuccessful(false);
+      return;
+    }
+
+    setIsWordValid(response.isWordValid);
+    setIsWordCheckSuccessful(true);
   };
 
   return (
