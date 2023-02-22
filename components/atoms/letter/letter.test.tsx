5af0122e4fe1a88a83c372f2ff05d2cd63425514
Sailesh Patel <saileshapatel@gmail.com>
2023-02-22 21:00:13 +0000

    Check letter state colour matches correct state

--- components/atoms/letter/letter.test.tsx
+++ components/atoms/letter/letter.test.tsx
@@ -52,4 +52,50 @@ describe("when a letter is entered", () => {
       expect(mockCheckIfWordIsValid).toBeCalledTimes(1);
     });
   });
+
+  describe("when checkLetterState is true", () => {
+    describe("and the entered letter matches the chosen word letter", () => {
+      it("the background color should match #5aff54b8", () => {
+        const renderedItem = render(
+          <Letter
+            letterIndex={1}
+            placeLetterInWord={jest.fn()}
+            checkIfWordIsValid={jest.fn()}
+            chosenWordLetter={"L"}
+            checkLetterState={true}
+          />
+        );
+
+        const { container } = renderedItem;
+        const input = renderedItem.getByLabelText("letter");
+
+        fireEvent.change(input, { target: { value: "L" } });
+        expect(container.children[0]).toHaveStyle(
+          "background-color: #5aff54b8"
+        );
+      });
+    });
+
+    describe("and the entered letter does not matches the chosen word letter", () => {
+      it("the background color should match #00110063", () => {
+        const renderedItem = render(
+          <Letter
+            letterIndex={1}
+            placeLetterInWord={jest.fn()}
+            checkIfWordIsValid={jest.fn()}
+            chosenWordLetter={"A"}
+            checkLetterState={true}
+          />
+        );
+
+        const { container } = renderedItem;
+        const input = renderedItem.getByLabelText("letter");
+
+        fireEvent.change(input, { target: { value: "L" } });
+        expect(container.children[0]).toHaveStyle(
+          "background-color: #00110063"
+        );
+      });
+    });
+  });
 });
